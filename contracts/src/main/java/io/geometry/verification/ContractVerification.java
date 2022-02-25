dcb2057cee29ed7416d68884f36187b13ff26475
robcxyz <rob@geometrylabs.io>
2022-02-25 08:23:39 -0800

    feat: add github source verfication options

--- contracts/src/main/java/io/geometry/verification/ContractVerification.java
+++ contracts/src/main/java/io/geometry/verification/ContractVerification.java
@@ -16,8 +16,6 @@
 
 package io.geometry.verification;
 
-import score.Address;
-import score.Context;
 import score.annotation.External;
 import score.annotation.EventLog;
 
@@ -38,6 +36,7 @@ public class ContractVerification {
         return version;
     }
 
+//    Simple pass through so we can collect the Tx inputs
     @External
     public void verify(
             String contract_address,
@@ -59,84 +58,22 @@ public class ContractVerification {
             String github,
             String keybase,
             String wechat,
+            String gradle_target,
+            String gradle_task,
+            String source_code_location,
+            String github_org,
+            String github_repo,
+            String github_directory,
+            String github_release,
             String zipped_source_code
     ) {
-        Address from_address = Context.getCaller();
-
-        // emit Verification event with all the input data acting simply as a pass through
-        Verification(
-                from_address,
-                version,
-                contract_address,
-                website,
-                team_name,
-                short_description,
-                long_description,
-                p_rep_address,
-                city,
-                country,
-                license,
-                facebook,
-                telegram,
-                reddit,
-                discord,
-                steemit,
-                twitter,
-                youtube,
-                github,
-                keybase,
-                wechat,
-                zipped_source_code
-        );
+        VerificationVersion(this.version);
     }
 
     @EventLog
-    protected void Verification(
-            Address from_address,
-            String version,
-            String contract_address,
-            String website,
-            String team_name,
-            String short_description,
-            String long_description,
-            String p_rep_address,
-            String city,
-            String country,
-            String license,
-            String facebook,
-            String telegram,
-            String reddit,
-            String discord,
-            String steemit,
-            String twitter,
-            String youtube,
-            String github,
-            String keybase,
-            String wechat,
-            String zipped_source_code
+    protected void VerificationVersion(
+//            Emit an event log for the version when the event happened so we can support evolving schemas
+            String version
     ) {
     }
 }
-
-
-//        this.contract_address = contract_address;
-//                this.email = email;
-//                this.website = website;
-//                this.team_name = team_name;
-//                this.short_description = short_description;
-//                this.short_description = short_description;
-//                this.p_rep_address = p_rep_address;
-//                this.city = city;
-//                this.country = country;
-//                this.license = license;
-//                this.facebook = facebook;
-//                this.telegram = telegram;
-//                this.reddit = reddit;
-//                this.discord = discord;
-//                this.steemit = steemit;
-//                this.twitter = twitter;
-//                this.youtube = youtube;
-//                this.github = github;
-//                this.keybase = keybase;
-//                this.wechat = wechat;
-//                this.zipped_source_code = zipped_source_code;
